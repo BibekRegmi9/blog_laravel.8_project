68fd20f5cb1aa4e57492314105a139d0090b2928
Bibek <vivekregmi83@gmail.com>
2024-01-03 15:29:28 +0545

    Post controller is added

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -22,6 +22,7 @@ class Post extends Model
     protected $with = ['category', 'author'];
 
 
+
     public function category(){
         return $this->belongsTo(Category::class);
 
--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,8 @@ Route::get('/', function(){
     $posts = Post::latest();
 
     if(request('search ')){
-        $posts->where('title', 'like', '%'. request('search') . '%' );
+        $posts->where('title', 'like', '%'. request('search') . '%' )
+            ->orWhere('body',  'like', '%'. request('search') . '%');
     }
 
     return view('posts', [
