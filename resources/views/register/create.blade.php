840e41119da065f58568d4c7afcaee874cd5926c
Bibek <vivekregmi83@gmail.com>
2024-01-04 14:27:26 +0545

    validation errors added  in registration form view like (name must be at least 3 character)

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -9,7 +9,11 @@
 
                     </label>
 
-                    <input class="border border-gray-400 p-2 w-full" type="text" name="name" id="name" required>
+                    <input class="border border-gray-400 p-2 w-full" type="text" name="name" id="name" value="{{ old('name') }}" required>
+
+                    @error('name')
+                        <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -17,7 +21,11 @@
 
                     </label>
 
-                    <input class="border border-gray-400 p-2 w-full" type="text" name="username" id="username" required>
+                    <input class="border border-gray-400 p-2 w-full" type="text" name="username" id="username" value="{{ old('username') }}" required>
+
+                    @error('username')
+                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -25,7 +33,11 @@
 
                     </label>
 
-                    <input class="border border-gray-400 p-2 w-full" type="email" name="email" id="email" required>
+                    <input class="border border-gray-400 p-2 w-full" type="email" name="email" id="email" value="{{ old('email') }}" required>
+
+                    @error('email')
+                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -33,7 +45,11 @@
 
                     </label>
 
-                    <input class="border border-gray-400 p-2 w-full" type="password" name="password" id="password" required>
+                    <input class="border border-gray-400 p-2 w-full" type="password" name="password" id="password" value="{{ old('password') }}" required>
+
+                    @error('password')
+                    <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
