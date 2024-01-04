5ef02dd3b480ed95a0365935a647ad1321489ad3
Bibek <vivekregmi83@gmail.com>
2024-01-04 15:47:21 +0545

    flash message component is created

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -59,13 +59,6 @@
     </footer>
 </section>
 
-@if(session() -> has('success'))
-    <div x-data = "{show: true}"
-         x-init="setTimeout(() => show = false, 4000)"
-         x-show = "show"
-        class="fixed bg-blue-500 text-white py-2 px-4 rounded-xl bottom-10 right-3 text-sm"
-    >
-        <p> {{ session('success') }} </p>
-    </div>
-@endif
+<x-flash/>
+
 </body>
