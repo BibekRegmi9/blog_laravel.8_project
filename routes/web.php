f1df29951484a5db5cd47a762c1829473e9c8633
Bibek <vivekregmi83@gmail.com>
2024-01-04 16:57:25 +0545

    login form view is created

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,7 @@ Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 Route::get('/register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('/register', [RegisterController::class, 'store'])->middleware('guest');
 
-Route::post('/login', [SessionController::class, 'create'])->middleware('guest');
+Route::get('/login', [SessionController::class, 'create'])->middleware('guest');
 Route::post('/logout', [SessionController::class, 'destroy'])->middleware('auth');
 
 
