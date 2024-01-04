bb97ff4ab1ea6c8d3d975588aeca214fef8fed53
Bibek <vivekregmi83@gmail.com>
2024-01-04 12:16:53 +0545

    pagination added

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,35 +6,13 @@
         @if($posts ->count())
 
             <x-posts-grid :posts="$posts"/>
+
+            {{$posts->links()}}
         @else
             <p class="text-center">No Posts Yet. Check Back later!!!</p>
         @endif
 
-        {{--        <div class="lg:grid lg:grid-cols-3">--}}
-        {{--            <x-post-card/>--}}
-        {{--            <x-post-card/>--}}
-        {{--            <x-post-card/>--}}
 
-        {{--        </div>--}}
     </main>
-
-
-    {{--    @foreach($posts as $post)--}}
-    {{--        <article>--}}
-    {{--            <h1>--}}
-    {{--                <a href="/posts/{{$post->slug}}">--}}
-    {{--                    {!! $post->title !!}--}}
-    {{--                </a>--}}
-
-    {{--            </h1>--}}
-    {{--            <p>--}}
-    {{--                By: <a href="/authors/{{$post-> author->username}}">{{$post->author->name}}</a> in: <a href="/categories/{{$post->category->slug}}"> {{$post->category->name}} </a>--}}
-    {{--            </p>--}}
-    {{--            <div>--}}
-    {{--                {{$post->excerpt}}--}}
-    {{--            </div>--}}
-    {{--        </article>--}}
-    {{--    @endforeach--}}
-
 
 </x-layout>
