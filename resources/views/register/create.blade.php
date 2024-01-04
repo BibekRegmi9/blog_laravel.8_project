b384ed32d59c22ed7114c0d7cd2c9c7eb06a191a
Bibek <vivekregmi83@gmail.com>
2024-01-04 13:44:02 +0545

    store method created to create the user when user clicks on the submit button

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -3,6 +3,7 @@
         <main class="max-w-lg mx-auto bg-gray-100 border border-gray-200 p-8 rounded-xl">
             <h1 class="text-center font-bold text-xl">Register!</h1>
             <form method="POST" action="/register" class="mt-10">
+                @csrf
                 <div class="mb-6">
                     <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="name"> Name
 
@@ -17,6 +18,14 @@
                     </label>
 
                     <input class="border border-gray-400 p-2 w-full" type="text" name="username" id="username" required>
+                </div>
+
+                <div class="mb-6">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="email"> Email
+
+                    </label>
+
+                    <input class="border border-gray-400 p-2 w-full" type="email" name="email" id="email" required>
                 </div>
 
                 <div class="mb-6">
