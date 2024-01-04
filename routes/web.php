ebe53548c874ac0b3f5f4dcd848c2ed7f27e8dd5
Bibek <vivekregmi83@gmail.com>
2024-01-04 16:11:53 +0545

    if logged in the register should dissapper from the layout views

--- routes/web.php
+++ routes/web.php
@@ -24,8 +24,8 @@ Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
 
-Route::get('/register', [RegisterController::class, 'create']);
-Route::post('/register', [RegisterController::class, 'store']);
+Route::get('/register', [RegisterController::class, 'create'])->middleware('guest');
+Route::post('/register', [RegisterController::class, 'store'])->middleware('guest');
 
 
 // route to get all posts which are associated with specific category
