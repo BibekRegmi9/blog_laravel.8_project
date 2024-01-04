5ec35882c9249c6a8c53dd965c319a99f1ecf81c
Bibek <vivekregmi83@gmail.com>
2024-01-04 12:38:28 +0545

    register controller added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
 use App\Models\Category;
@@ -22,6 +23,8 @@ Route::get('/', [PostController::class, 'index'])->name('home');
 
 
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('/register', [RegisterController::class, 'create']);
 
 
 // route to get all posts which are associated with specific category
