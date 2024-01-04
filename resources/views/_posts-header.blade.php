e18cfd96668c44984b1e957380280ae94cd7d278
Bibek <vivekregmi83@gmail.com>
2024-01-04 09:52:56 +0545

    category dropdown component is added

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -10,15 +10,12 @@
     <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-8">
         <!--  Category -->
                 <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
-
+                    <x-category-dropdown/>
 
        <x-dropdown>
-
            <x-slot name="trigger">
                <button class="py-2 pl-3 pr-9 text-sm font-semibold w-32 text-left inline-flex ">
-
                    {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'categories'}}
-
                    <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
                         height="22" viewBox="0 0 22 22">
                        <g fill="none" fill-rule="evenodd">
@@ -28,18 +25,14 @@
                                  d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z"></path>
                        </g>
                    </svg>
-
                </button>
            </x-slot>
-
            <a href="/" class="block text-left px-3 text-xm leading-6 hover:bg-gray-300 focus:bg-gray-300 "> All:  </a>
-
            @foreach($categories as $category)
                <a href="/?categories={{$category->slug}}"
                   class="block text-left px-3 text-xm leading-6 hover:bg-gray-300 focus:bg-gray-300
                        {{isset($currentCategory) && $currentCategory->id === $category->id ? 'bg-blue-500 text-white' : ''}}
                        "> {{ucwords($category->name)}} </a>
-
            @endforeach
        </x-dropdown>
 
