2fae081478f0e0dc3b8ed70d7594a000fe2809bb
Bibek <vivekregmi83@gmail.com>
2024-01-05 12:53:22 +0545

    post comment controller added

---
+++ app/Http/Controllers/PostCommentsController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostCommentsController extends Controller
+{
+    //
+
+//    public function store(Post $post){
+//
+//    }
+}
