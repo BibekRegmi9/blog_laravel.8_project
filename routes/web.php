90c2015e603258700a23b8e6ccb23b12020cec56
Bibek <vivekregmi83@gmail.com>
2024-01-11 09:57:08 +0545

    route for patch is added

--- routes/web.php
+++ routes/web.php
@@ -63,6 +63,7 @@ Route::get('/admin/posts', [AdminPostController::class, 'index'])->middleware('a
 Route::get('/admin/posts/create', [AdminPostController::class, 'create'])->middleware('admin');
 Route::post('/admin/posts', [AdminPostController::class, 'store'])->middleware('admin');
 Route::get('/admin/posts/{post}/edit', [AdminPostController::class, 'edit'])->middleware('admin');
+Route::patch('/admin/posts/{post}', [AdminPostController::class, 'update'])->middleware('admin');
 
 
 // route to get all posts which are associated with specific category
