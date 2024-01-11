cff707a1b7d15bbcf60947845360af724ff6cdf8
Bibek <vivekregmi83@gmail.com>
2024-01-11 10:07:52 +0545

    Post edit- completed

--- app/Http/Controllers/AdminPostController.php
+++ app/Http/Controllers/AdminPostController.php
@@ -45,12 +45,30 @@ class AdminPostController extends Controller
 
         Post::create($attributes);
 
-        return redirect('/');
+        return redirect('/')->with('success', 'Post Added!');
     }
 
     public function edit(Post $post){
         return view('admin.posts.edit',[
             'post' => $post
         ]);
+    }
+
+    public function update(Post $post){
+        $attributes = request()->validate([
+            'title' => 'required',
+            'thumbnail' => 'image',
+            'slug' => ['required', Rule::unique('posts', 'slug')->ignore($post->id)],
+            'excerpt' => 'required',
+            'body' => 'required',
+            'category_id' => ['required', Rule::exists('categories', 'id')]
+        ]);
+
+        if(isset($attributes['thumbnail'])){
+            $attributes['thumbnail'] = request()->file('thumbnail')->store('thumbnails');
+        }
+
+        $post -> update($attributes);
+        return redirect('/admin/posts')->with('success', 'Post Updated!');
     }
 }
